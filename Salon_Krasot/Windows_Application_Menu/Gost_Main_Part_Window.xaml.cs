c0a082675284ebb37f16786f64daaaf60f0b7c95
NeKrOmAnT1 <miftahov9548@gmail.com>
2023-06-19 21:42:47 +0300

    Problems

--- Salon_Krasot/Windows_Application_Menu/Gost_Main_Part_Window.xaml.cs
+++ Salon_Krasot/Windows_Application_Menu/Gost_Main_Part_Window.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore;
 using Salon_Krasot.Models;
 
 namespace Salon_Krasot.Windows_Application_Menu
@@ -21,22 +22,21 @@ namespace Salon_Krasot.Windows_Application_Menu
     /// </summary>
     public partial class Gost_Main_Part_Window : Window
     {
+        private ApplicationContext dbContext;
         public ObservableCollection<Product_Card> Products { get; set; }
         public Gost_Main_Part_Window()
         {
             InitializeComponent();
-            Products = new ObservableCollection<Product_Card>();
-            Katalog_lb.ItemsSource = Products;
-
-            Products.Add(new Product_Card { Title = "ТЕСТ", Price = 999 });
-            Products.Add(new Product_Card { Title = "Тест", Price = 849 });
-            Products.Add(new Product_Card { Title = "ТеСт", Price = 1312 });
-            Products.Add(new Product_Card { Title = "Туз", Price = 5435 });
-            Products.Add(new Product_Card { Title = "Тестирование", Price = 4234 });
-            Products.Add(new Product_Card { Title = "Снова тестирование", Price = 3132 });
-            Products.Add(new Product_Card { Title = "Черт побери, откуда тут взялся туз¿", Price = 3132, Manufacturer="adawgaa" });
+            DataContext = this;
+            dbContext= new ApplicationContext();
+            LoadProducts();
         }
-        
+
+        private void LoadProducts()
+        {
+            Products = new ObservableCollection<Product_Card>(dbContext.Products_Cards.ToList());
+            Katalog_lb.ItemsSource = Products;
+        }
 
         private void btn_choose_Click(object sender, RoutedEventArgs e)
         {
