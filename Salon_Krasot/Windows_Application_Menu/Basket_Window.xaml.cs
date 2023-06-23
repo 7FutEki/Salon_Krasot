6bd62ab1015f68ff3f101ae7685f1c7374ea3604
NeKrOmAnT1 <miftahov9548@gmail.com>
2023-06-23 12:24:17 +0300

    Basket_FIX

--- Salon_Krasot/Windows_Application_Menu/Basket_Window.xaml.cs
+++ Salon_Krasot/Windows_Application_Menu/Basket_Window.xaml.cs
@@ -48,15 +48,15 @@ namespace Salon_Krasot.Windows_Application_Menu
                 }
 
                 Products = new ObservableCollection<Product_Card>(basket_product);
-                
+
                 foreach (var photo in Products)
                 {
-                    if (photo.Photo.Length ==58)
+                    if (photo.Photo.Length == 58)
                     {
                         continue;
                     }
                     else
-                    photo.Photo = $"pack://application:,,,/{photo.Photo}";
+                        photo.Photo = $"pack://application:,,,/{photo.Photo}";
                 }
 
 
@@ -80,13 +80,42 @@ namespace Salon_Krasot.Windows_Application_Menu
 
             }
         }
-       
+
 
         private void btn_exit_Click(object sender, RoutedEventArgs e)
         {
             User_Main_Part_Window user_Main_Part_Window = new User_Main_Part_Window();
             Close();
             user_Main_Part_Window.ShowDialog();
+        }
+
+        private void buy_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (Basket_lb.SelectedItem != null)
+            {
+                MessageBox.Show("К сожалению, на данный момент невозможно выполнить данную операцию. Рекомендуется обратиться в службу поддержки для получения дополнительной помощи. Мы приносим извинения за возможные неудобства.", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Warning   );
+            }
+        }
+
+        private void delete_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (Basket_lb.SelectedItem != null)
+            {
+                var selectedProduct = (Product_Card)Basket_lb.SelectedItem;
+                using (var db = new ApplicationContext())
+                {
+                    var logins = db.ForLogin.ToList();
+                    string login = logins.Last().Login;
+                    var basketProduct = db.Basket_Product_Cards.FirstOrDefault(x => x.Title == selectedProduct.Title && x.Login == login);
+
+                    if (basketProduct != null)
+                    {
+                        db.Basket_Product_Cards.Remove(basketProduct);
+                        db.SaveChanges();
+                        Products.Remove(selectedProduct);
+                    }
+                }
+            }
         }
     }
 }
